1ea8781f288d00d4c1d163fda09767f9f907abd6
Vinicius S Sanchez <viniciusanchez@gmail.com>
2020-03-20 21:52:48 -0300

    Update database.js
    Remove password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password: 'Vssdev123+',
+    password: 'XXX',
     database: 'sqlnode',
     define: {
         timestamps: true,
